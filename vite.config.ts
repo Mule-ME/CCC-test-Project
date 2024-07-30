bb519bea07b10839981dad2fda4b0d4c38391290
Mule-ME <mulualemesh15@gmail.com>
2024-07-30 10:57:49 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3002,
+    host: true,
+  },
+  preview: {
+    port: 3002,
+  },
+  resolve: {
+    alias: {
+      src: "/src",
+      api: "/src/api",
+      assets: "/src/assets",
+      components: "/src/components",
+      layout: "/src/layout",
+      pages: "/src/pages",
+      providers: "/src/providers",
+      store: "/src/store",
+      styles: "/src/styles",
+      utils: "/src/utils",
+      hooks: "/src/hooks",
+      models: "/src/models",
+    },
+  },
+});
