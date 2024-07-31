c4727fbdb30d58e7306bb3ca2f6605b1562b69c3
Mulualem <mulualemesh15@gmail.com>
2024-07-31 11:02:47 +0300

    feat: Appbar and sidebar implmentation

--- src/components/layouts/AppBar.tsx
+++ src/components/layouts/AppBar.tsx
@@ -1,60 +1,104 @@
+import * as React from "react";
 import {
-  Typography,
-  Grid,
-  Toolbar,
   AppBar as DefaultAppBar,
-  Badge,
+  ListItemIcon,
+  Box,
+  Toolbar,
   IconButton,
+  Typography,
+  Menu,
+  Avatar,
+  Button,
+  Tooltip,
+  MenuItem,
 } from "@mui/material";
-// import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
-const AppBar = () => {
+import { appBarLeftMenu, appBarProfileMenu, appBarRightMenu } from "./config";
+import { Settings } from "@mui/icons-material";
+
+function AppBar() {
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <DefaultAppBar position="fixed">
+    <DefaultAppBar sx={{ backgroundColor: "#232" }}>
       <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          onClick={handleDrawerOpen}
-          edge="start"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Grid item xs={12}>
-          <Typography variant="h6" noWrap>
-            <img className={classes.logo} src={Logo} alt="Help Ex Login Logo" />
-          </Typography>
-        </Grid>
-        <Popper
-          className={classes.popper}
-          open={openNotification}
-          anchorEl={anchorEls}
-          placement={placement}
-          transition
-        >
-          {({ TransitionProps }) => (
-            <Fade {...TransitionProps} timeout={5}>
-              <Notification />
-            </Fade>
-          )}
-        </Popper>
-        <div className={classes.cons}>
-          <Badge badgeContent={17} color="error">
-            <NotificationsIcon
-              onClick={handleClickNotification("bottom-start")}
-              className={classes.Navicons}
-              fontSize="large"
-            />
-          </Badge>
-          <SettingsIcon className={classes.Navicons} fontSize="large" />
-          <AccountCircleIcon
-            className={classes.Navicons}
-            fontSize="large"
-            onClick={handleProfileMenuOpen}
-          />
-        </div>
+        <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+          <IconButton sx={{ p: 0 }}>
+            <Avatar
+              alt="Fingroo"
+              src=""
+              sx={{ width: 24, height: 24, fontSize: 15 }}
+            >
+              F
+            </Avatar>
+          </IconButton>
+
+          {appBarLeftMenu?.map((menu) => (
+            <Button sx={{ textTransform: "capitalize" }}>
+              <Typography color="#fff" sx={{ fontSize: 15 }}>
+                {menu?.title}
+              </Typography>
+              <ListItemIcon sx={{ minWidth: "unset", ml: 1 }}>
+                {menu?.icon}
+              </ListItemIcon>
+              {/* {menu?.separator} */}
+            </Button>
+          ))}
+        </Box>
+
+        <Box sx={{ flexGrow: 0 }}>
+          {appBarRightMenu?.map((rightMenu) => (
+            <Button sx={{ textTransform: "capitalize" }}>
+              <Typography color="#fff" sx={{ fontSize: 15 }}>
+                {rightMenu?.title}
+              </Typography>
+            </Button>
+          ))}
+
+          <Tooltip title="Settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Settings fontSize="medium" sx={{ color: "#fff" }} />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Mulualem Eshetu" src="" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {appBarProfileMenu.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
       </Toolbar>
     </DefaultAppBar>
   );
-};
-
+}
 export default AppBar;
--- src/components/layouts/SideBar.tsx
+++ src/components/layouts/SideBar.tsx
@@ -1,5 +1,96 @@
+import { useState } from "react";
+import {
+  Box,
+  Drawer,
+  CssBaseline,
+  List,
+  ListItemButton,
+  Collapse,
+  Typography,
+} from "@mui/material";
+import { sideBarResources } from "./config";
+import AppBar from "./AppBar";
+
 const SideBar = () => {
-  return <div>SideBar</div>;
+  // const [open, setOpen] = useState<Record<string, boolean>>({});
+
+  // const handleClick = (title: string) => {
+  //   setOpen((prevOpen: Record<string, boolean>) => ({
+  //     ...prevOpen,
+  //     [title]: !prevOpen[title],
+  //   }));
+  // };
+
+  const [open, setOpen] = useState<Record<string, boolean>>(
+    sideBarResources.reduce((acc, resource) => {
+      acc[resource.title] = false;
+      return acc;
+    }, {} as Record<string, boolean>)
+  );
+
+  const handleClick = (title: string) => {
+    setOpen((prevOpen: Record<string, boolean>) => {
+      const newOpen: Record<string, boolean> = {};
+      for (const key in prevOpen) {
+        newOpen[key] = key === title ? !prevOpen[key] : false;
+      }
+      return newOpen;
+    });
+  };
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar />
+
+      <Drawer
+        sx={{
+          width: 240,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: 240,
+            boxSizing: "border-box",
+            marginTop: 8,
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        {sideBarResources.map((res, index) => (
+          <List
+            key={index}
+            sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+            component="nav"
+          >
+            <ListItemButton
+              onClick={() => handleClick(res.title)}
+              sx={{
+                backgroundColor: open[res.title]
+                  ? "rgba(0, 0, 0, 0.08)"
+                  : "transparent",
+              }}
+            >
+              {res.icon}
+              <Typography sx={{ fontSize: 15, pl: 2 }}>{res.title}</Typography>
+            </ListItemButton>
+            {res.children.length > 0 && (
+              <Collapse in={open[res.title]} timeout="auto" unmountOnExit>
+                <List component="div">
+                  {res.children.map((child, childIndex) => (
+                    <ListItemButton key={childIndex} sx={{ pl: 5 }}>
+                      <Typography sx={{ fontSize: 14, pl: 2 }}>
+                        {child.title}
+                      </Typography>{" "}
+                    </ListItemButton>
+                  ))}
+                </List>
+              </Collapse>
+            )}
+          </List>
+        ))}
+      </Drawer>
+    </Box>
+  );
 };
 
 export default SideBar;
